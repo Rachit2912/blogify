59b7f4e8884d0b552a4ba9850f6b9cadae99b957
Rachit2912 <rachitsandeepjoshi@gmail.com>
2024-10-06 21:13:51 +0530

    update images

--- routes/blog.js
+++ routes/blog.js
@@ -25,6 +25,8 @@ router.get("/add-new", (req, res) => {
   });
 });
 
+
+
 router.get("/:id", async (req, res) => {
   const blog = await Blog.findById(req.params.id).populate("createdBy");
   const comments = await Comment.find({ blogId: req.params.id }).populate(
